5dd3b63c6e2d337fbc7464047e8254474fd13625
Naomi-Eckhoff <nsuibhne@gmail.com>
2022-01-26 13:26:48 -0600

    c

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -42,7 +42,6 @@ const handlePizzaSubmit = event => {
   const pizzaName = $pizzaForm.querySelector('#pizza-name').value;
   const createdBy = $pizzaForm.querySelector('#created-by').value;
   const size = $pizzaForm.querySelector('#pizza-size').value;
-  // transform DOM data into a real array of objects to execute .map()
   const toppings = [...$pizzaForm.querySelectorAll('[name=topping]:checked')].map(topping => {
     return topping.value;
   });
@@ -56,7 +55,7 @@ const handlePizzaSubmit = event => {
   fetch('/api/pizzas', {
     method: 'POST',
     headers: {
-      Accept: 'application/jsonn',
+      Accept: 'application/json',
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(formData)
